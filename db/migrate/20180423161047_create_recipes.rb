d7b73268f0ba78272bf5f1e4a06231c08eadba93
zaynab bou <zaynabourdellah@hotmail.com>
2018-04-23 17:20:02 +0100

    creation model recipe

---
+++ db/migrate/20180423161047_create_recipes.rb
@@ -0,0 +1,17 @@
+class CreateRecipes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :recipes do |t|
+      t.text :title
+      t.text :effect
+      t.time :time
+      t.integer :difficulty
+      t.integer :cost
+      t.string :purpose
+      t.text :description
+      t.text :state
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
