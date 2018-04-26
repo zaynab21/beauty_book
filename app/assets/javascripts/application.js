7d7f3cd55b0cd6e9f324dffebec41acf9e8cbdcd
Amine Bensaid <aminebac13@gmail.com>
2018-04-26 18:10:41 +0100

    es6 js fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,7 +4,7 @@
 const fat = document.querySelector(".fat");
 const filter = document.querySelector(".container-filters");
 
-fat.addEventListener("click",(event) => {
+fat.addEventListener("click",function(event) {
   if (filter.style.display == "block"){
     filter.style.display = "none";
   } else {
