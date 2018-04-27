c001cdd016ee7d32707c05861ac097e5517b9f7c
Amine Bensaid <aminebac13@gmail.com>
2018-04-27 12:01:33 +0100

    fixed js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,14 @@
 import "bootstrap";
 import { initUpdateNavbarOnScroll } from '/components/navbar';
 initUpdateNavbarOnScroll();
+
+const fat = document.querySelector(".fat");
+const filter = document.querySelector(".container-filters");
+
+fat.addEventListener("click",function(event) {
+  if (filter.style.display == "block"){
+    filter.style.display = "none";
+  } else {
+    filter.style.display = "block";
+  }
+})
