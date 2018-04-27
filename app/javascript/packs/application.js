a28cc03d48a8793d44cc96604c23df58ad8c0176
Amine Bensaid <aminebac13@gmail.com>
2018-04-27 12:16:32 +0100

    fixing js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,6 @@
 
 import "bootstrap";
-import { initUpdateNavbarOnScroll } from '/components/navbar';
+import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
 
 const fat = document.querySelector(".fat");
