4f7b93601b28e189769d6291584c9596fd693088
Amine Bensaid <aminebac13@gmail.com>
2018-04-27 12:04:14 +0100

    saved a dumb file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,5 @@
 //= require rails-ujs
 //= require_tree
 
-const fat = document.querySelector(".fat");
-const filter = document.querySelector(".container-filters");
-
-fat.addEventListener("click",function(event) {
-  if (filter.style.display == "block"){
-    filter.style.display = "none";
-  } else {
-    filter.style.display = "block";
-  }
-})
 
 
