227c16386b02501586448f249d330e7c65ec3aa6
Amine Bensaid <aminebac13@gmail.com>
2018-04-30 17:37:08 +0100

    checkboxes on filters

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :recipes
   has_many :reviews
+  mount_uploader :photo, PhotoUploader
   validates :username, presence: true, uniqueness: true
   validates :email, uniqueness: true
   validates :age, numericality: { only_integer: true }, allow_blank: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20180430115452) do
-
+ActiveRecord::Schema.define(version: 20180430150133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,9 +51,9 @@ ActiveRecord::Schema.define(version: 20180430115452) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "views", default: 0
     t.integer "application_time_min"
     t.integer "preparation_time_min"
-    t.integer "views", default: 0
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
@@ -106,6 +104,7 @@ ActiveRecord::Schema.define(version: 20180430115452) do
     t.text "skin_type"
     t.text "hair_type"
     t.text "description"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
