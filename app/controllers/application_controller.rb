25bb8f0ec231bca07939450654f27e2d9eea245d
zaynab bou <zaynabourdellah@hotmail.com>
2018-05-02 11:25:13 +0100

    fix sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
    def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :age])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :age, :skin_type, :hair_type, :photo, :description])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :photo])
