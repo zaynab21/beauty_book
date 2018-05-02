aa1d1f9e3aa9cf15e6f19fc73cf5b81c29cd3c4a
Amine Bensaid <aminebac13@gmail.com>
2018-05-02 16:42:28 +0100

    Remove category to make heroku work

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -11,7 +11,6 @@ class Recipe < ApplicationRecord
   validates_associated :tags
   validates :title, presence: true
   validates :state, presence:true
-  validates :category, presence: true
   #inclusion
   pg_search_scope :global_search,
     against: [ :title, :feature, :difficulty, :cost, :category, :description ],
